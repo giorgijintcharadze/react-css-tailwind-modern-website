b59686173ddd84972699d9339344e9b61dfad8fd
giorgijintcharadze <giorgijintcharadze7@gmail.com>
2025-12-19 18:01:27 +0400

    created LandingPage

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+
+const App = () => {
+  return (
+    <div className="bg-slate-950 min-h-screen text-white overflow-hidden  ">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
